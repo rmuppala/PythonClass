c5bc12165a600a3677e17867c8c26acfdacdae7f
rmuppala <35551332+rmuppala@users.noreply.github.com>
2018-02-02 21:04:54 -0600

    Add files via upload

--- Lab1/src/SentenseLab1.py
+++ Lab1/src/SentenseLab1.py
@@ -1,9 +1,10 @@
+#define processSentence function
 def processSentence(sentence ):
     print(sentence)
     words = sentence.split()
     n = len(words)
 
-    #print middle words
+    #Iterate through words print 1 to n-1 Middle words in the sentense
     print("\nMiddle words in the sentense : ", end ="")
     for i in  range(1,n-1):
          print( words[i] , end= " ")
@@ -11,6 +12,7 @@ def processSentence(sentence ):
     #find longest word
     x = 0
     lword = 0
+    #Iterate through all the words compare length and set identify index of the long word
     for i in range(0,n):
         if (lword < len(words[i])):
              lword = len(words[i])
@@ -18,10 +20,12 @@ def processSentence(sentence ):
 
     print('\nLongest word : ' , words[x])
 
-    #print words in reverse order
+    #Iterate through all the words use print each word in reverse
     print('Sentense with reverse words : ',  end= "")
     for i in range(0,n):
         print(words[i][::-1] , end= " ")
 
+#Accept sentese from User
 senten = input("Enter sentense : ")
+#call function to processSentense
 processSentence(senten)
